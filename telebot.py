169cea233291e6a253724fbf81ee9649b1fc0c54
Truong An <79464700+Pawnzr@users.noreply.github.com>
2024-04-26 10:20:46 +0700

    Update telebot.py

--- telebot.py
+++ telebot.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot('YOUR_TOKEN')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Xin chào! Đây là bot tính toán đơn giản. Gửi bất kỳ phép tính nào dưới dạng `/sum 3,1` để tính tổng.")
+    bot.reply_to(message, "Xin chào!")
 
 
 bot.polling()
